d3867ea64a10d2f717175a655e92929017a2c97c
hskim <andykim881028@gmail.com>
2021-07-05 01:07:40 +0900

    Clean up

--- Assets/Scripts/Factory/DataFactory.cs
+++ Assets/Scripts/Factory/DataFactory.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Fighter.Clone;
 using Fighter.Data;
@@ -10,15 +11,16 @@ namespace Fighter.Factory {
             switch (data.CloneType) {
                 case CloneType.Player:
                     var player = data as CharacterDataScriptableObject;
-                    return player.Data.Cast<IData>().ToList();;
+                    return player.Data.Cast<IData>().ToList();
                 case CloneType.Enemy:
                     var enemy = data as CharacterDataScriptableObject;
-                    return enemy.Data.Cast<IData>().ToList();;
+                    return enemy.Data.Cast<IData>().ToList();
                 case CloneType.Projectile:
                     var projectile = data as ProjectileDataScriptableObject;
-                    return projectile.Data.Cast<IData>().ToList();;
+                    return projectile.Data.Cast<IData>().ToList();
+                default:
+                    throw new ArgumentOutOfRangeException();
             }
-            return null;
         }
     }
 }
