bc5ee14fa85e3666303fb98f9376cbc7912650e2
hskim <andykim881028@gmail.com>
2021-07-08 00:19:56 +0900

    Remove unused values

--- Assets/Scripts/Data/BaseData.cs
+++ Assets/Scripts/Data/BaseData.cs
@@ -5,8 +5,6 @@ namespace Fighter.Data {
     [Serializable]
     public class BaseData : IData
     {
-        public Vector3 Position;
-        public Vector3 Direction;
         public Vector3 Look;
         public float Speed;
         public int Damage;
